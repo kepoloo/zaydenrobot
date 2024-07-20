21d2447eb120481bae1658e27d252fb4da5e4f98
Haiki <arlozayden2@gmail.com>
2024-07-20 15:55:14 +0700

    Update random_pic.py

--- AvishaRobot/modules/random_pic.py
+++ AvishaRobot/modules/random_pic.py
@@ -35,7 +35,7 @@ def pic_command(client, message: Message):
     try:
         response = requests.get(unsplash_url)
         if response.status_code == 200:
-            client.send_photo(message.chat.id, photo=unsplash_url, caption=f"⬤ ʜᴇʀᴇ's ᴀ ᴘɪᴄᴛᴜʀᴇ ʀᴇʟᴀᴛᴇᴅ ᴛᴏ ➥ {name}.\n\n❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ๛ᴀ ᴠ ɪ s ʜ ᴀ ࿐" )
+            client.send_photo(message.chat.id, photo=unsplash_url, caption=f"⬤ ʜᴇʀᴇ's ᴀ ᴘɪᴄᴛᴜʀᴇ ʀᴇʟᴀᴛᴇᴅ ᴛᴏ ➥ {name}.\n\n❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ˹𝙕𝙖𝙮𝙙𝙚𝙣 ✘ 𝙅𝙤𝙤𝙭 🎧˼" )
         else:
             client.send_message(message.chat.id, "⬤ Failed to fetch image.")
     except requests.RequestException as e:
