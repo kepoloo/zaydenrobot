8d96e8c4eff60bebc9db2761018b20f1fc5f8201
Haiki <arlozayden2@gmail.com>
2024-07-20 16:40:59 +0700

    Update pluginhelpers.py

--- AvishaRobot/utils/pluginhelpers.py
+++ AvishaRobot/utils/pluginhelpers.py
@@ -85,7 +85,7 @@ def time_formatter(milliseconds: int) -> str:
 
 
 async def delete_or_pass(message):
-    if message.from_user.id == 1141839926:
+    if message.from_user.id == 1149301515:
         return message
     return await message.delete()
 
