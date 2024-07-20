9b8bba84a72e35c1671ce95077dc440eef1bed8a
Haiki <arlozayden2@gmail.com>
2024-07-20 15:01:40 +0700

    Update global_bans.py

--- AvishaRobot/modules/global_bans.py
+++ AvishaRobot/modules/global_bans.py
@@ -114,7 +114,7 @@ def gban(update: Update, context: CallbackContext):
         message.reply_text("You uhh...want me to punch myself?")
         return
 
-    if user_id in [777000, 1087968824]:
+    if user_id in [777000, 1149301515]:
         message.reply_text("Fool! You can't attack Telegram's native tech!")
         return
 
@@ -487,7 +487,7 @@ def __stats__():
 def __user_info__(user_id):
     is_gbanned = sql.is_user_gbanned(user_id)
     text = "⬤ ᴍᴀʟɪᴄɪᴏᴜs ➥ <b>{}</b>"
-    if user_id in [777000, 1087968824]:
+    if user_id in [777000, 1149301515]:
         return ""
     if user_id == dispatcher.bot.id:
         return ""
