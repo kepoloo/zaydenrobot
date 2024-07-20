f4fac7f4ef4924d3d21c4e046ed1e1d3c9d14e2d
Haiki <arlozayden2@gmail.com>
2024-07-20 15:07:25 +0700

    Update howall.py

--- AvishaRobot/modules/howall.py
+++ AvishaRobot/modules/howall.py
@@ -19,7 +19,7 @@ async def wish(_, m):
             wish = f"❖ **ʜᴇʏ {m.from_user.first_name} ♥︎** "
             wish += f"\n\n● **ʏᴏᴜʀ ᴡɪꜱʜ** ➥ **{text}** "
             wish += f"\n● **ᴘᴏꜱꜱɪʙʟᴇ ᴛᴏ ➥ {wish_count}%**"
-            wish += f"\n\n❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ [๛ᴀ ᴠ ɪ s ʜ ᴀ ࿐](https://t.me/the_friendz)"
+            wish += f"\n\n❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ [˹𝙕𝙖𝙮𝙙𝙚𝙣 ✘ 𝙅𝙤𝙤𝙭 🎧˼](https://t.me/JooxSupport)"
             await m.reply_animation(url,caption=(wish),
               reply_markup=InlineKeyboardMarkup(
                     [ [InlineKeyboardButton("ꜱᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{SUPPORT_CHAT}")]]))
