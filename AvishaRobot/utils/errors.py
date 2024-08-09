e820b66b62d6080ffab1c38b76d20661032dfd88
Haiki <arlozayden2@gmail.com>
2024-08-09 15:05:04 +0700

    Update errors.py

--- AvishaRobot/utils/errors.py
+++ AvishaRobot/utils/errors.py
@@ -4,7 +4,7 @@ from functools import wraps
 
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-from AvishaRobot import OWNER_ID, pbot
+from AarohiX import OWNER_ID, pbot
 
 
 def split_limits(text):
